c5e388f0bc12cb1b407fbe62bfa7ae25cba4a29b
akblazeacademy <AKBLAZEACADEMY@GMAIL.COM>
2025-11-30 23:41:54 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,1 +1,12 @@
+output "rg_name" {
+  value = azurerm_resource_group.rg.name
+}
+
+output "vnet_name" {
+  value = azurerm_virtual_network.vnet.name
+}
+
+output "subnet_name" {
+  value = azurerm_subnet.subnet.name
+}
 
